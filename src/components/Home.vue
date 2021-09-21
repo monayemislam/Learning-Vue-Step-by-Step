7526894556fb564dd920129b0150b3e2bbbbfd06
Mahfuz Shuvo <monayemislam@gmail.com>
2021-09-21 15:45:50 +0600

    Interpolation and Data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,26 @@
 <template>
-    <h1>Hello I am a new component</h1>
+    <div>
+        <h1>Example of InterPolation and Data</h1>
+        <h3>{{myName}}</h3>
+        <h3>{{homeTown}}</h3>
+        <h3>Lenght of my name (Monayem):{{"Monayem".length}}</h3>
+    </div>
 </template>
 
 <script>
     export default{
-        name: "Home"
+        name: "Home",
+        data(){
+            return {
+                myName:"Monayem Islam",
+                homeTown: "Chapainawabganj"
+            }
+        }
     }
 </script>
 
 <style scoped>
-    h1{
+    div{
         color: skyblue;
         border: 2px solid skyblue;
     }
