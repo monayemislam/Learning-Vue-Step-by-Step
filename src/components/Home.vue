ad4b963b39c4ada4de233ba797edcdd9a058ba91
Mahfuz Shuvo <monayemislam@gmail.com>
2021-09-21 15:07:42 +0600

    Home component is added

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello I am a new component</h1>
+</template>
+
+<script>
+    export default{
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: skyblue;
+        border: 2px solid skyblue;
+    }
+</style>
